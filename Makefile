ff781bf6fd727928a20ae86ed20e7f31d6e937e2
Matt Melling <mattmelling@fastmail.com>
2022-12-23 18:01:28 +0000

    update fake6502, switch to pkg-config

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=gcc
-CFLAGS=-g -Werror -pedantic -I include -O1
+CFLAGS=-g -Werror -pedantic -I include -O1 $(shell pkg-config --libs --cflags --static fake65c02)
 LD=ld
 
 OBJ=main.o mm6502.o memory.o repl.o dism.o via.o acia.o
@@ -10,10 +10,9 @@ default: mm6502
 %.o: %.c $(HEADERS)
 	$(CC) $(CFLAGS) -c $< -o $@
 
-mm6502: lib/fake65c02.o $(OBJ)
-	$(CC) $(CFLAGS) $^ -o $@
+mm6502: $(OBJ)
+	$(CC) $(CFLAGS) -l:fake6502.o $^ -o $@
 
 .PHONY: clean
 clean:
 	rm -f *.o mm6502
-
--- flake.nix
+++ flake.nix
@@ -6,17 +6,11 @@
     devShells.x86_64-linux.default = let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [ pkg-config ];
       buildInputs = with pkgs; [
-        pkg-config
         cc65
         fake6502.packages.x86_64-linux.fake6502
       ];
-      # hack: I can't figure out how to expose fake6502 properly
-      shellHook = ''
-        rm lib include
-        ln -fs ${fake6502.packages.x86_64-linux.fake6502}/lib lib
-        ln -fs ${fake6502.packages.x86_64-linux.fake6502}/include include
-      '';
     };
   };
 }
